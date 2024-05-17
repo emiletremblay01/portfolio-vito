3eb110e3d40d2ae3b09b7d6a0a3bbaddbf787193
Emile Tremblay <emiletremblay01@gmail.com>
2024-05-16 20:29:08 -0400

    fix: updated experience types

---
+++ types.ts
@@ -0,0 +1,7 @@
+export type Experience = {
+  title: string;
+  experienceCategory: "postproduction" | "evenementiel" | "composition";
+  experienceType: "professionelle" | "personnelle" | "scolaire";
+
+  image: string;
+};
