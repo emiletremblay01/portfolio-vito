3ad2c9269b3fbf0f8d0314fa06eac16478267e59
Emile Tremblay <emiletremblay01@gmail.com>
2024-05-16 20:58:24 -0400

    feat: :art: added separators to ui

--- components/navExperiences.tsx
+++ components/navExperiences.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 import Link from "next/link";
-
+import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 
 import { useSearchParams, useRouter } from "next/navigation";
@@ -48,21 +48,27 @@ export function NavExperiences({
   return (
     <>
       <nav className="">
-        <div className="pb-2 text-lg font-bold text-black/80">Expériences</div>
+        <div className="mb-1 text-lg font-bold text-black/80">Expériences</div>
+
         <div className="flex w-full flex-wrap gap-x-2">
           {routes.map((route) => (
-            <Link
-              key={route.label}
-              href={`?experience=${route.type}`}
-              className={cn(
-                "text-sm font-medium transition-colors hover:text-[#4E6B33]",
-                route.active
-                  ? "text-[#4E6B33] underline decoration-inherit underline-offset-1 dark:text-white"
-                  : "text-black/80",
-              )}
-            >
-              {route.labelShort}
-            </Link>
+            <div key={route.label} className="group flex">
+              <Link
+                href={`?experience=${route.type}`}
+                className={cn(
+                  "text-sm font-medium transition-colors hover:text-[#4E6B33]",
+                  route.active
+                    ? "text-[#4E6B33] underline decoration-inherit underline-offset-1 dark:text-white"
+                    : "text-black/80",
+                )}
+              >
+                {route.labelShort}
+              </Link>
+              <Separator
+                orientation="vertical"
+                className="ml-2 bg-black/20 group-last:hidden"
+              />
+            </div>
           ))}
         </div>
       </nav>
