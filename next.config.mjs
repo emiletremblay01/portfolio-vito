95eca2790a70bfb12a64b11e824e5dab96503186
Emile Tremblay <emiletremblay01@gmail.com>
2024-06-22 17:45:25 -0400

    fix: fixed the image domains settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["picsum.photos"],
+    remotePatterns: [
+      {
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
 };
 
